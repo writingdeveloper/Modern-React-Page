dfebb90717180fb9e704dccc065bf3eb31c4e172
Si Hyeong Lee <super2451894@gmail.com>
2022-05-12 13:39:53 +0900

    Add Navbar Components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
-// import "./navbar.css";
+import "./navbar.css";
 
 // BEM -> Block Element Modifier
 
@@ -11,6 +11,23 @@ const Navbar = () => {
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
         </div>
       </div>
     </div>
