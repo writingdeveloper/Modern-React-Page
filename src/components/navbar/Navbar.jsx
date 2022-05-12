20e9c93cf8e54000e1f9b9affd50cfc4bdc24f04
Si Hyeong Lee <super2451894@gmail.com>
2022-05-12 13:46:13 +0900

    Add navbar Style Codes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
-        <div className="gpt3__navbar-links_links_container">
+        <div className="gpt3__navbar-links_container">
           <p>
             <a href="#home">Home</a>
           </p>
